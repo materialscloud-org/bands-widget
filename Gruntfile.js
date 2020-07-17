d6c3780c56cabd39af4f01470cf34b59ed32588e
Snehal Kumbhar <snehal.kumbhar@epfl.ch>
2020-07-17 18:30:48 +0200

    cleanup; updated gruntfile;

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,92 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    meta: {
-      banner: '// Materials Cloud bands structure widget v<%= pkg.version %>\n' +
-          '// <%= grunt.template.today("yyyy-mm-dd") %>, MIT License\n'
-    },
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        meta: {
+            banner: '// Materials Cloud bands structure widget v<%= pkg.version %>\n' +
+                '// https://www.materialscloud.org\n' +
+                '// Contributors: <%= pkg.author %>\n' +
+                '// (c) <%= grunt.template.today("yyyy") %>, Released under the MIT License\n'
+        },
 
-    uglify: {
-      options: {
-        mangle: true,
-        banner: '<%= meta.banner %>'
-      },
-      dist: {
-        files: {
-          'dist/mcloud-bands-widget.min.js': ['js/bandstructure.js']
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: "dist/{,*/}*"
+                }]
+            }
+        },
+
+        concat: {
+            options: {
+                separator: ';',
+                nonull: true
+            },
+            dist: {
+                src: [
+                    // 'js/tinycolor.min.js',
+                    // 'js/Chart.min.js',
+                    // 'js/hammer.min.js',
+                    // 'js/chartjs-plugin-zoom.min.js',
+                    'js/bandstructure.min.js'],
+                dest: 'dist/bandstructure.min.js'
+            }
+        },
+
+        // minifies JS files
+        uglify: {
+            options: {
+                mangle: true,
+                banner: '<%= meta.banner %>'
+            },
+            dist: {
+                files: {
+                    'dist/bandstructure.min.js': ['js/bandstructure.js']
+                }
+            }
+        },
+
+        babel: {
+            compile: {
+                options: {
+                    sourceMap: true,
+                    presets: ['@babel/preset-env', 'babel-preset-es2015']
+                },
+                dist: {
+                    files: {
+                        'dist/bandstructure.min.js': ['js/bandstructure.js']
+                    }
+                }
+            }
+        },
+
+        jshint: {
+            options: {
+                browser: true,
+                sub: true,
+                globals: {
+                    jQuery: true
+                }
+            },
+            all: {
+                src: ['js/bands.js', 'js/bandstructure.js']
+            }
         }
-      }
-    },
-
-    jshint: {
-      options: {
-        browser: true,
-        sub: true,
-        globals: {
-          jQuery: true
-        }
-      },
-      all: {
-        src: ['js/bands.js', 'js/bandstructure.js']
-      }
-    }
-  });
+    });
 
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('build', ['jshint', 'uglify']);
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-babel');
+
+    grunt.registerTask('build', [
+        "clean",
+        'jshint',
+        'babel',
+        'concat'
+    ]);
 
 };
