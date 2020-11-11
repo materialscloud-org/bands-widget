d1429a70220748917a83ab030476b4b3d2a3789d
Dou <dou.du.epfl@gmail.com>
2020-11-11 19:33:45 +0100

    fix the zoom bug

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,9 +26,9 @@ module.exports = function(grunt) {
             dist: {
                 src: [
                      'js/chartjs.min.js',
+                     'js/chartjs-plugin-zoom.min.js',
                      'js/tinycolor.min.js',
                      'js/hammer.min.js',
-                     'js/chartjs-plugin-zoom.min.js',
                     'dist/bandstructure.min.js'],
                 dest: 'dist/bandstructure.min.js'
             }
